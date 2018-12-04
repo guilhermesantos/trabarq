b080cd3925a6e9d6a3ff9bb1a241beba84a15fa0
guilhermesantos <guilherme.milan9@gmail.com>
2018-12-04 01:17:03 -0200

    adaptacao nos scripts

--- auto_tests.sh
+++ auto_tests.sh
@@ -1,31 +1,30 @@
 echo 'Fazendo testes de multiplicacao de matrizes inteiras'
-
 PLAT_NAME=$1
 for i in {1..10}
 	do
-		sudo perf stat ./bench_int_mat &>> result_int_mat_$PLAT_NAME.txt
+		sudo perf stat -d ./bench_int_mat &>> result_int_mat_$PLAT_NAME.txt
 	done
 
 echo 'Fazendo testes de multiplicacao de matrizes float'
 for i in {1..10} 
 	do
-		sudo perf stat ./bench_float_mat &>> result_float_mat_$PLAT_NAME.txt
+		sudo perf stat -d ./bench_float_mat &>> result_float_mat_$PLAT_NAME.txt
 	done
 
 echo 'Fazendo testes de gauss'
 for i in {1..10} 
 	do
-		sudo perf stat ./bench_float_gauss &>> result_float_gauss_$PLAT_NAME.txt
+		sudo perf stat -d ./bench_float_gauss &>> result_float_gauss_$PLAT_NAME.txt
 	done
 
 echo 'Fazendo testes de branch sem sort'
 for i in {1..10} 
 	do
-		sudo perf stat ./bench_branch &>> result_branch_$PLAT_NAME.txt
+		sudo perf stat -d ./bench_branch &>> result_branch_$PLAT_NAME.txt
 	done
 
 echo 'Fazendo testes de branch com sort'
 for i in {1..10} 
 	do
-		sudo perf stat ./bench_branch_sort &>> result_branch_sort_$PLAT_NAME.txt
+		sudo perf stat -d ./bench_branch_sort &>> result_branch_sort_$PLAT_NAME.txt
 	done
