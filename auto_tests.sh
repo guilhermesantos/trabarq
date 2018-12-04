7872b649b1fbabb1b5300afaceb88b35c6269d32
guilhermesantos <guilherme.milan9@gmail.com>
2018-12-04 00:32:16 -0200

    script auto_tests.sh

--- auto_tests.sh
+++ auto_tests.sh
@@ -17,13 +17,13 @@ for i in {1..10}
 		sudo perf stat ./bench_float_gauss &>> result_float_gauss_i5.txt
 	done
 
-echo 'Fazendo testes de multiplicacao de branch sem sort'
+echo 'Fazendo testes de branch sem sort'
 for i in {1..10} 
 	do
 		sudo perf stat ./result_branch_i5 &>> result_branch_i5.txt
 	done
 
-echo 'Fazendo testes de multiplicacao de branch com sort'
+echo 'Fazendo testes de branch com sort'
 for i in {1..10} 
 	do
 		sudo perf stat ./result_branch_sort_i5 &>> result_branch_i5.txt
