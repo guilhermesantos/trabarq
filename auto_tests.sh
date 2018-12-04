56b5e8f144226e30ce658f5d53d0f1d209246840
guilhermesantos <guilherme.milan9@gmail.com>
2018-12-04 00:42:00 -0200

    alteracao do script pra pegar nome da plataforma da linha de comando

--- auto_tests.sh
+++ auto_tests.sh
@@ -1,30 +1,31 @@
 echo 'Fazendo testes de multiplicacao de matrizes inteiras'
 
+PLAT_NAME=$1
 for i in {1..10}
 	do
-		sudo perf stat ./bench_int_mat &>> result_int_mat_i5.txt
+		sudo perf stat ./bench_int_mat &>> result_int_mat_$PLAT_NAME.txt
 	done
 
 echo 'Fazendo testes de multiplicacao de matrizes float'
 for i in {1..10} 
 	do
-		sudo perf stat ./bench_float_mat &>> result_float_mat_i5.txt
+		sudo perf stat ./bench_float_mat &>> result_float_mat_$PLAT_NAME.txt
 	done
 
 echo 'Fazendo testes de gauss'
 for i in {1..10} 
 	do
-		sudo perf stat ./bench_float_gauss &>> result_float_gauss_i5.txt
+		sudo perf stat ./bench_float_gauss &>> result_float_gauss_$PLAT_NAME.txt
 	done
 
 echo 'Fazendo testes de branch sem sort'
 for i in {1..10} 
 	do
-		sudo perf stat ./result_branch_i5 &>> result_branch_i5.txt
+		sudo perf stat ./bench_branch &>> result_branch_$PLAT_NAME.txt
 	done
 
 echo 'Fazendo testes de branch com sort'
 for i in {1..10} 
 	do
-		sudo perf stat ./result_branch_sort_i5 &>> result_branch_i5.txt
+		sudo perf stat ./bench_branch_sort &>> result_branch_$PLAT_NAME.txt
 	done
